c9eee71fe4520dd7f1247ba2651493f4e9ce6415
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2024-03-29 21:43:35 +0100

    mojdoj

--- Inner Shadows/Assets/Scripts/Camera/CameraControler.cs
+++ Inner Shadows/Assets/Scripts/Camera/CameraControler.cs
@@ -7,10 +7,19 @@ public class CameraControler : MonoBehaviour
     [SerializeField] private float aheadDistance;
     [SerializeField] private float verticalOffset;
     [SerializeField] private float damping;
-    [SerializeField] private float look_down;
+    [SerializeField] private float lookDown;
+    [SerializeField] private Camera cam;
+
+    private FearOfLost lostMeter;
     private float lookAhead;
     private Vector3 targetPosition;
 
+    private float camStart = 23.50195f;
+    private float transitionSpeed = 2f;
+    private void Start()
+    {
+        lostMeter = GameObject.FindObjectOfType<FearOfLost>();
+    }
     private void Update()
     {
         // Horizontal camera movement
@@ -19,14 +28,37 @@ public class CameraControler : MonoBehaviour
 
         // Vertical camera movement
         float targetYPosition = player.position.y + verticalOffset;
+        float meter = lostMeter.fearMeter.fillAmount;
 
+        // fear of lost cam, zooming
+        if (meter <= 0.3f)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camStart, Time.deltaTime * transitionSpeed);
+            targetYPosition = player.position.y + verticalOffset;
+
+        }
+        else if (meter <= 0.5f)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 20f, Time.deltaTime * transitionSpeed);
+            targetYPosition -= 2f;
+
+        }
+        else if (meter <= 0.8f)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 13f, Time.deltaTime * transitionSpeed);
+            targetYPosition -= 6f;
+        }
+        else if (meter <= 1f)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 10f, Time.deltaTime * transitionSpeed);
+            targetYPosition -= 10f;
+        }
         // Check if the "S" key is being held down
         if (Input.GetKey(KeyCode.S))
         {
             // Move the camera down
-            targetYPosition -= look_down; // Adjust this value as needed for desired downward movement speed
+            targetYPosition -= lookDown; // Adjust this value as needed for desired downward movement speed
         }
-
         targetPosition.y = Mathf.Lerp(transform.position.y, targetYPosition, Time.deltaTime * damping);
 
         transform.position = targetPosition;
